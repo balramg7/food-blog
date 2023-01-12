ddee4128a992b722477e490b1807b9e360924a37
Balram Gupta <109209618+balramg7@users.noreply.github.com>
2023-01-12 17:10:24 +0530

    add blogpost

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from '../styles/Blog.module.css'
+import Link from 'next/link'
 
 const Blog = () => {
   return (
-    <div>
-      This is a Blog Home Page
+    <div className={styles.container}>
+      <main className={styles.main}>
+    <div className={styles.blogItem}>
+      <Link href={'/blogpost/eat-alootikki'}>
+      <h3>Ganpat Chat Bhandar</h3></Link>
+      <p>This Shop is famous for Aloo Tikki</p>
+    </div>
+    <div className={styles.blogItem}>
+      <h3>Kunjilal Sweet Corner</h3>
+      <p>This Shop is famous for different variety of sweet</p>
+    </div>
+    <div className={styles.blogItem}>
+      <h3>Mangla ChamCham Corner</h3>
+      <p>This Shop is famous for ChamCham</p>
+    </div>
+    </main>
     </div>
   )
 }
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,14 +1,37 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
-    const router = useRouter();
-    const {slug} = router.query;
+  const router = useRouter();
+  const { slug } = router.query;
   return (
-    <div>
-      {slug}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.h1}>Title of the blog page {slug}</h1>
+        <hr />
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          obcaecati quidem aliquid? Vero quisquam ratione obcaecati quae dolorum
+          nemo quia laboriosam eligendi. Qui officiis fugit consequatur facilis,
+          vero totam recusandae?
+        </div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        fuga qui sequi, aut beatae commodi. Adipisci voluptatum ab ipsam aut
+        placeat temporibus labore, odit molestiae harum quaerat vitae corporis
+        perferendis natus perspiciatis saepe quas illo nostrum dolores aperiam
+        consequuntur, veniam qui? Perspiciatis necessitatibus sit saepe ullam
+        amet! Officiis perferendis provident iure dolorem fuga, aspernatur
+        corrupti dolores possimus non assumenda reiciendis ullam, molestiae eum
+        sapiente sit voluptate dolorum molestias unde itaque. Ipsum odit
+        consectetur, hic distinctio deserunt aperiam modi similique, vitae
+        nostrum pariatur labore commodi! Aliquid assumenda quibusdam dolore
+        numquam cum ducimus atque officia fugit magnam porro veritatis, nihil
+        accusantium dolorem!
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default slug
+export default slug;
